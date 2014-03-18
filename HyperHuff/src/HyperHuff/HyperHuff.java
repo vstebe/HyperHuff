9890156c7b799c44617ab46ff085b0ab2bf25090
Vincent Stébé <vincent.stebe@gmail.com>
2014-03-18 13:44:58 +0100

    ajout tableentry

--- HyperHuff/src/HyperHuff/HyperHuff.java
+++ HyperHuff/src/HyperHuff/HyperHuff.java
@@ -1,13 +1,18 @@
 package HyperHuff;
 
+import image.GreyImage;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class HyperHuff {
 
 	public static void main(String[] args) {
 		
-		Node[] nodes = new Node[] {
+		/*Node[] nodes = new Node[] {
 				new Node((short)0, 1), //a
 				new Node((short)1, 1), //c
 				new Node((short)2, 1),//s
@@ -23,7 +28,44 @@ public class HyperHuff {
 		
 		Huffman h = new Huffman(new ArrayList<Node>(Arrays.asList(nodes)));
 		
-		h.process();
+		h.process();*/
+		
+		
+		GreyImage img;
+		try {
+			img = GreyImage.loadPGM("/home/vincent/barbara.pgm");
+			
+			HashMap<Short, Integer> caracters = new HashMap<Short, Integer>();
+			for(int i=0; i<img.getSizeData(); i++)
+			{
+				Short s = img.getPixel(i);
+				if(caracters.containsKey(s))
+					caracters.put(s, caracters.get(s)+1);
+				else
+					caracters.put(s, 0);
+			}
+			
+			Node[] nodes = new Node[caracters.size()];
+			int i=0;
+			for(Entry<Short, Integer> entry : caracters.entrySet())
+			{
+				nodes[i] = new Node(entry.getKey(), entry.getValue());
+				i++;
+			}
+			
+			
+			
+			Huffman h = new Huffman(new ArrayList<Node>(Arrays.asList(nodes)));
+			h.process();
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 
 	}
 
---
+++ HyperHuff/src/HyperHuff/TableEntry.java
@@ -0,0 +1,12 @@
+package HyperHuff;
+
+public class TableEntry {
+	public short value;
+	public String code;
+	
+	public TableEntry(short v, String code)
+	{
+		value = v;
+		this.code = code;
+	}
+}
